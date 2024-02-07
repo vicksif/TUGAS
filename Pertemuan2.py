56ba6235655eda27f7b94d6e298237fb9e012e15
vicksif <158256987+vicksif@users.noreply.github.com>
2024-02-07 13:51:31 +0700

    Update Pertemuan2.py

--- Pertemuan2.py
+++ Pertemuan2.py
@@ -1,1 +1,20 @@
+import streamlit as st
 
+# Header
+st.header('Vicky San :Sparkles:')
+st.subheader('Plot')
+
+c1, c2 = st.coulums(2)
+
+with c1:
+  x = st.number_imput('Suhu',value=100)
+  st.write('=>: ')
+with c2:
+  satuan = st.selectbox(
+    'Satuan',
+    ('+', '-', 'x', ':'),key='k1')
+  st.werite(':sparkles: ')
+
+st.write(x,' ',satuan,' = ',' ')
+
+st.caption(Copyright © Vicky Sandika Putra Firdiawajn 210322607233')
